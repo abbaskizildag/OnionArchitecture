36dbc168a894e4ed5f9276546e375d5a355c697c
Ali Kızıldağ <ali.kizildag@cottgroup.com>
2022-10-03 16:28:24 +0300

    Add project files.

---
+++ src/Core/ProductApp.Application/Parameters/RequestParameter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Application.Parameters
+{
+    public class RequestParameter
+    {
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+        public RequestParameter(int pagaSize, int pageNumber)
+        {
+            PageSize = pageNumber;
+            PageNumber = pagaSize;
+        }
+    }
+}
---
+++ src/WebApi/ProductApp.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Application.Dto;
+using ProductApp.Application.Interfaces.Repository;
+
+namespace ProductApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRespository productRespository;
+        public ProductController(IProductRespository productRespository)
+        {
+            this.productRespository = productRespository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var allList = await productRespository.GetAllAsync();
+
+            var result = allList.Select(i => new ProductViewDto()
+            {
+                Id = i.Id,
+                Name = i.Name,
+            });
+
+            return Ok(result);
+        }
+    }
+}
